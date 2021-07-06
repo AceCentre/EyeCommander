c65a73167940c543f39a97760213b3f25e01d86e
Daniel Kashkett <Dkashkett@gmail.com>
2021-07-06 10:03:07 -0600

    refresh now returns output from camera. seperate function added for returning eye images from frame. better for debugging and easier to read.

--- DeepLearning_Pipeline/commander.py
+++ DeepLearning_Pipeline/commander.py
@@ -19,7 +19,7 @@ class EyeCommander:
     DEFAULT_MODEL = tf.keras.models.load_model('./models/jupiter2')
     
     def __init__(self, model=None, window_size: int =12):
-        self.cam = cv2.VideoCapture(0)
+        self.camera = cv2.VideoCapture(0)
         self.calibration = calibration.Calibration()
         self.eye_detection = eye_detection.EyeDetector()
         self.prediction_window = classification.PredictionWindow(size=window_size)
@@ -32,20 +32,40 @@ class EyeCommander:
         self.eye_status = False
         self.cam_status = False 
         self.calibration_done = False
-
-    def _refresh(self):
-        self.cam_status, self.frame = self.cam.read()
+         
+    def refresh(self):
+        cam_status, frame = self.camera.read()
         # set the flag to improve performance
-        self.frame.flags.writeable = False
+        frame.flags.writeable = False
         # Stop if no video input
-        if self.cam_status == True:
-            self.shape = self.frame.shape
+        if cam_status == True:
+            self.shape = frame.shape
             # create display frame 
-            self.display_frame = cv2.flip(self.frame, 1)
-            self.display_frame.flags.writeable = True
-            # extract eye images
-            self.eye_left, self.eye_right = self.eye_detection.extract_eyes(self.frame)
-            self.eye_status = self.eye_detection.status
+            display_frame = cv2.flip(frame, 1)
+            display_frame.flags.writeable = True
+            return (display_frame, frame)
+        else:
+            return None
+
+    def _eye_images_from_frame(self, frame):
+        eyes = self.eye_detection.extract_eyes(frame)
+        if eyes:
+            return eyes
+        else:
+            return None
+    
+    def _preprocess_eye_images(self, eye_left:np.array, eye_right:np.array, shape:tuple = (100,100,1)):
+        # gray 
+        gray_left = cv2.cvtColor(eye_left, cv2.COLOR_BGR2GRAY)
+        gray_right =  cv2.cvtColor(eye_right, cv2.COLOR_BGR2GRAY)
+        # resize
+        resized_left = cv2.resize(gray_left, shape[:2])
+        resized_right = cv2.resize(gray_right, shape[:2])
+        # reshape
+        reshaped_left = resized_left.reshape(shape)
+        reshaped_right = resized_right.reshape(shape)
+        
+        return reshaped_left, reshaped_right
 
     def _prep_batch(self, images: tuple):
         img1 = images[0].reshape((100,100,1))
@@ -55,8 +75,8 @@ class EyeCommander:
         batch = np.concatenate([img1,img2])
         return batch
     
-    def _predict_batch(self, batch):
-        if self.callibration_done == True:
+    def _predict_batch(self, batch:np.array):
+        if self.calibration_done == True:
             predictions = self.model.predict(batch) 
         else:
              predictions = self.DEFAULT_MODEL.predict(batch)
@@ -70,56 +90,74 @@ class EyeCommander:
             predictions = self._predict_batch(batch)
             # add both predictions to the prediction window
             self.prediction_window.insert_predictions(predictions)
-    
+            
+    def predict(self, eye_left:np.array, eye_right:np.array):
+        # make batch
+        batch = self._prep_batch((eye_left, eye_right))
+        # make prediction on batch
+        predictions = self._predict_batch(batch)
+        # add both predictions to the prediction window
+        self.prediction_window.insert_predictions(predictions)
         # make prediction over a window 
         pred, mean_proba = self.prediction_window.predict()
         return pred, mean_proba
              
-    def _display_prediction(self, label, color = (252, 198, 3), font=cv2.FONT_HERSHEY_PLAIN):
+    def _display_prediction(self, label, frame, color = (252, 198, 3), font=cv2.FONT_HERSHEY_PLAIN):
         if label == 'left':
-            cv2.putText(self.display_frame, "left", (50, 375), font , 7, color, 15)
+            cv2.putText(frame, "left", (50, 375), font , 7, color, 15)
         elif label == 'right':
-            cv2.putText(self.display_frame, "right", (900, 375), font, 7, color, 15)
+            cv2.putText(frame, "right", (900, 375), font, 7, color, 15)
         elif label == 'up':
-            cv2.putText(self.display_frame, "up", (575, 100), font, 7, color, 15)
+            cv2.putText(frame, "up", (575, 100), font, 7, color, 15)
         else:
-            cv2.putText(self.display_frame, "down", (500, 700), font, 7, color, 15)
+            cv2.putText(frame, "down", (500, 700), font, 7, color, 15)
         # display frame
-        cv2.imshow("frame", self.display_frame)
-       
+        # cv2.imshow("frame", frame)
+    
     def run(self, calibrate=True):
         if calibrate == True:
             self.model = self.calibration.calibrate() 
             self.calibration_done == True 
-        while self.cam.isOpened():
+        while self.camera.isOpened():
             # capture a frame and extract eye images
-            self._refresh()
-            if (self.cam_status == False) or (self.eye_status == False):
-                continue
-            # display suggested head placement box
-            cv2.rectangle(self.display_frame,(420,20),(900,700),(100,175,255),3)
-            cv2.putText(self.display_frame, "center head inside box", 
+            camera_output = self.refresh()
+            # if the camera is outputing images
+            if camera_output:
+                display_frame, frame = camera_output
+                # detect eyes
+                eyes = self._eye_images_from_frame(frame)
+                # if detection is able to isolate eyes
+                if eyes:
+                    eye_left, eye_right = eyes
+                    eye_left_processed, eye_right_processed = self._preprocess_eye_images(eye_left, eye_right)
+                    # make classifcation based on eye images
+                    prediction, mean_proba = self.predict(eye_left_processed, eye_right_processed) 
+                    # display results
+                    if (self.prediction_window.is_full() == True) and (mean_proba > 2):
+                        label = self.CLASS_LABELS[prediction]
+                        self._display_prediction(label=label, frame=display_frame)
+                cv2.imshow('eye', eye_left_processed)
+                    # self._display_prediction(label, display_frame)
+                # display suggested head placement box
+                cv2.rectangle(display_frame,(420,20),(900,700),(100,175,255),3)
+                cv2.putText(display_frame, "center head inside box", 
                         (470, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (100,175,255), 1)
-            # make classifcation based on eye images
-            prediction, mean_proba = self._classify() 
-            # display results
-            if (self.prediction_window.is_full() == True) and (mean_proba > 8):
-                label = self.CLASS_LABELS[prediction]
-                self._display_prediction(label)
-            else:
-                cv2.imshow('frame', self.display_frame)
+                cv2.imshow('display', display_frame)
             # end demo when ESC key is entered 
             if cv2.waitKey(5) & 0xFF == 27:
                 shutil.rmtree('./temp/data')
                 break
                 
-        self.cam.release()
+        self.camera.release()
         cv2.destroyAllWindows()
     
 if __name__ == "__main__":
     # model = tf.keras.models.load_model('./models/jupiter2')
     commander = EyeCommander()
-    commander.run(calibrate=True)
+    commander.run(calibrate=False)
+    # c = calibration.Calibration()
+    # c.gen_data(direction='up',n_frames=50)
+    # pass
     
     
 
