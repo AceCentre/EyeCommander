27be1e76f4c44814a9c9ae1d17f16e7aaec3e6f9
Daniel Kashkett <Dkashkett@gmail.com>
2021-07-06 10:22:43 -0600

    eye image shape is now a class attribute that can be changed easily

--- DeepLearning_Pipeline/commander.py
+++ DeepLearning_Pipeline/commander.py
@@ -14,24 +14,18 @@ from classification import classification
 
 class EyeCommander:
     CLASS_LABELS = ['center', 'down', 'left', 'right', 'up']
-    SOUND  = pyglet.media.load("./sounds/ding.mp3", streaming=False)
     TEMP_DATAPATH = './temp'
-    DEFAULT_MODEL = tf.keras.models.load_model('./models/jupiter2')
+    BASE_MODEL_PATH = './models/jupiter2'
     
-    def __init__(self, model=None, window_size: int =12):
+    def __init__(self, model=None, window_size: int =12, image_shape:tuple =(100,100,1)):
         self.camera = cv2.VideoCapture(0)
         self.calibration = calibration.Calibration()
         self.eye_detection = eye_detection.EyeDetector()
         self.prediction_window = classification.PredictionWindow(size=window_size)
         self.window_size = window_size
-
         self.model = model
-        self.frame = None
-        self.display_frame = None
-        self.shape = None
-        self.eye_status = False
-        self.cam_status = False 
         self.calibration_done = False
+        self.image_shape = image_shape
          
     def refresh(self):
         cam_status, frame = self.camera.read()
@@ -39,7 +33,6 @@ class EyeCommander:
         frame.flags.writeable = False
         # Stop if no video input
         if cam_status == True:
-            self.shape = frame.shape
             # create display frame 
             display_frame = cv2.flip(frame, 1)
             display_frame.flags.writeable = True
@@ -54,7 +47,8 @@ class EyeCommander:
         else:
             return None
     
-    def _preprocess_eye_images(self, eye_left:np.array, eye_right:np.array, shape:tuple = (100,100,1)):
+    def _preprocess_eye_images(self, eye_left:np.array, eye_right:np.array):
+        shape = self.image_shape
         # gray 
         gray_left = cv2.cvtColor(eye_left, cv2.COLOR_BGR2GRAY)
         gray_right =  cv2.cvtColor(eye_right, cv2.COLOR_BGR2GRAY)
@@ -68,8 +62,8 @@ class EyeCommander:
         return reshaped_left, reshaped_right
 
     def _prep_batch(self, images: tuple):
-        img1 = images[0].reshape((100,100,1))
-        img2 = images[1].reshape((100,100,1))
+        img1 = images[0].reshape(self.image_shape)
+        img2 = images[1].reshape(self.image_shape)
         img1 = np.expand_dims(img1,0)
         img2 = np.expand_dims(img2,0)
         batch = np.concatenate([img1,img2])
@@ -79,7 +73,7 @@ class EyeCommander:
         if self.calibration_done == True:
             predictions = self.model.predict(batch) 
         else:
-             predictions = self.DEFAULT_MODEL.predict(batch)
+             predictions = self.model.predict(batch)
         return predictions   
             
     def predict(self, eye_left:np.array, eye_right:np.array):
@@ -107,6 +101,8 @@ class EyeCommander:
         if calibrate == True:
             self.model = self.calibration.calibrate() 
             self.calibration_done == True 
+        else:
+            self.model = tf.keras.models.load_model(self.BASE_MODEL_PATH)
         while self.camera.isOpened():
             # capture a frame and extract eye images
             camera_output = self.refresh()
@@ -122,9 +118,10 @@ class EyeCommander:
                     # make classifcation based on eye images
                     prediction, mean_proba = self.predict(eye_left_processed, eye_right_processed) 
                     # display results
-                    if (self.prediction_window.is_full() == True) and (mean_proba > 2):
+                    if (self.prediction_window.is_full() == True) and (mean_proba > 4):
                         label = self.CLASS_LABELS[prediction]
                         self._display_prediction(label=label, frame=display_frame)
+                        
                 cv2.imshow('eye', eye_left_processed)
                     # self._display_prediction(label, display_frame)
                 # display suggested head placement box
@@ -143,7 +140,7 @@ class EyeCommander:
 if __name__ == "__main__":
     # model = tf.keras.models.load_model('./models/jupiter2')
     commander = EyeCommander()
-    commander.run(calibrate=False)
+    commander.run(calibrate=False)   
     # c = calibration.Calibration()
     # c.gen_data(direction='up',n_frames=50)
     # pass
