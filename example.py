8869e8921f09f75754577ad4aaab889a1b050652
Daniel Kashkett <Dkashkett@gmail.com>
2021-07-12 13:12:18 -0600

    delted temp data

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ from eye_commander.commander.commander import EyeCommander
 from eye_commander.utils.utils import dir_to_eye_images, video_dir_to_frames
 
 
-
-ec = EyeCommander(calibrate=True)
+#### To capture data run eyecommander with keep_data = True
+ec = EyeCommander(calibrate=True, keep_data=True)
 ec.run()   
 
 
