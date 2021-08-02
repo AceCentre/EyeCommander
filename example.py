25376653ab91c6805d658d0fd678aecd98c2bd25
Daniel Kashkett <dkashkett@gmail.com>
2021-08-02 10:59:16 -0600

    changed default to config off for example script

--- example.py
+++ example.py
@@ -2,5 +2,5 @@ from eye_commander.commander.commander import EyeCommander
 
 #### To capture data run eyecommander with keep_data = True
 ec = EyeCommander()
-ec.run(calibrate=True)   
+ec.run(calibrate=False)   
 
