4e23a964697bd84793b7c499c12bf41a999d22c4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-05 15:56:05 +0100

    Add a entry.py

---
+++ entry.py
@@ -0,0 +1,7 @@
+from eye_commander.commander.commander import EyeCommander
+from eye_commander.utils.utils import dir_to_eye_images, video_dir_to_frames
+
+
+#### To capture data run eyecommander with keep_data = True
+ec = EyeCommander(calibrate=True, keep_data=False)
+ec.run()
