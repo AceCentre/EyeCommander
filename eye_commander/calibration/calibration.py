3d54e63de237127bdd1b4a14a4b1926d0c7b733e
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-08-13 08:50:27 +0100

    Copy function accross places its needed just to get it working

--- eye_commander/calibration/calibration.py
+++ eye_commander/calibration/calibration.py
@@ -1,13 +1,25 @@
 from eye_commander.image_capture import image_capture
 from eye_commander.face_detection import face_detection
 from eye_commander.models import models
-from eye_commander.utils import resource_path
 import os
 import shutil
 import cv2
 import glob
 import numpy as np
 import tensorflow as tf
+import sys
+
+# Calculate the path to a resource.
+# If we are running in pyinstaller then the _MEIPASS env variable
+# is set so we set the path to that path
+# If its not set we assume you are just running it in normal python so use the
+# relative path to the resources
+# https://stackoverflow.com/questions/56210408/location-of-the-added-files-after-the-executable-file-is-generated-by-pyinstalle
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path.replace("/", "\\"))
+
+    return os.path.join(os.getcwd(), relative_path)
 class Calibrator:
     TEMP_PATH = resource_path('eye_commander/temp')
     CLASS_LABELS = ['center', 'down', 'left', 'right', 'up']
--- eye_commander/commander/commander.py
+++ eye_commander/commander/commander.py
@@ -5,10 +5,22 @@ from eye_commander.prediction_window import prediction_window
 from eye_commander.display_tools import display
 from eye_commander.calibration import calibration
 from eye_commander.keystroke import keystroke 
-from eye_commander.utils import resource_path
 import cv2 
 import numpy as np
 import os
+import sys
+
+# Calculate the path to a resource.
+# If we are running in pyinstaller then the _MEIPASS env variable
+# is set so we set the path to that path
+# If its not set we assume you are just running it in normal python so use the
+# relative path to the resources
+# https://stackoverflow.com/questions/56210408/location-of-the-added-files-after-the-executable-file-is-generated-by-pyinstalle
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path.replace("/", "\\"))
+
+    return os.path.join(os.getcwd(), relative_path)
 
 class EyeCommander:
     
