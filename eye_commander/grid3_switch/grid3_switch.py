a489dba54a668406ac25ab5d9c52f05bf4c8cb0c
will wade <willwade@gmail.com>
2021-08-17 11:55:13 +0100

    adding grid3 switch stuff.

---
+++ eye_commander/grid3_switch/grid3_switch.py
@@ -0,0 +1,44 @@
+from sys import platform
+if platform =="win32":
+	import win32api, win32con, win32gui, win32ui, win32service, os, time
+	import sys
+
+"""
+Sends switch presses on Grid3. Only possible on Windows and using Grid3. 
+
+Switch_Press(1)
+Switch_Release(1)
+
+Switch_Press_Release(2)
+Switch_Press_Release(3)
+"""
+
+if platform =="win32":
+	_wmSensorySwitchInput = win32gui.RegisterWindowMessage("Sensory_SwitchInput")
+
+def SetSwitchSate(id,state):
+	win32gui.SendMessage(win32con.HWND_BROADCAST,_wmSensorySwitchInput,id,state)
+
+def Switch_Press(id):
+	if platform =="win32":    
+		SetSwitchSate(id,0)
+	else:
+		return None;
+
+def Switch_Release(id):
+	if platform =="win32":    
+		SetSwitchSate(id,1)
+	else:
+		return None;
+
+def Switch_Press_Release(id):
+	if platform =="win32":    
+		SetSwitchSate(id,0)
+		SetSwitchSate(id,1)
+	else:
+		return None;
+
+# args = sys.argv[1:]
+# id = args[0]
+# state = args[1]
+
