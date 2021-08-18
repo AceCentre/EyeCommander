373755b6dd67587de58b4cd65f16efb3015ab126
will wade <willwade@gmail.com>
2021-08-18 10:05:03 +0100

    changing imports to deal with import errors

--- eye_commander/grid3_switch/grid3_switch.py
+++ eye_commander/grid3_switch/grid3_switch.py
@@ -1,7 +1,9 @@
 from sys import platform
-if platform =="win32":
-	import win32api, win32con, win32gui, win32ui, win32service, os, time
-	import sys
+if platform == "win32":
+    from win32 import win32gui
+    import win32ui
+    import win32con
+    import win32api
 
 """
 Sends switch presses on Grid3. Only possible on Windows and using Grid3. 
@@ -13,32 +15,39 @@ Switch_Press_Release(2)
 Switch_Press_Release(3)
 """
 
-if platform =="win32":
-	_wmSensorySwitchInput = win32gui.RegisterWindowMessage("Sensory_SwitchInput")
+if platform == "win32":
+    _wmSensorySwitchInput = win32gui.RegisterWindowMessage(
+        "Sensory_SwitchInput")
 
-def SetSwitchSate(id,state):
-	win32gui.SendMessage(win32con.HWND_BROADCAST,_wmSensorySwitchInput,id,state)
+
+def SetSwitchSate(id, state):
+    win32gui.SendMessage(win32con.HWND_BROADCAST,
+                         _wmSensorySwitchInput, id, state)
+
 
 def Switch_Press(id):
-	if platform =="win32":    
-		SetSwitchSate(id,0)
-	else:
-		return None;
+    if platform == "win32":
+        SetSwitchSate(id, 0)
+    else:
+        return None
+
 
 def Switch_Release(id):
-	if platform =="win32":    
-		SetSwitchSate(id,1)
-	else:
-		return None;
+    if platform == "win32":
+        SetSwitchSate(id, 1)
+    else:
+        return None
+
 
 def Switch_Press_Release(id):
-	if platform =="win32":    
-		SetSwitchSate(id,0)
-		SetSwitchSate(id,1)
-	else:
-		return None;
+    if platform == "win32":
+        SetSwitchSate(id, 0)
+        SetSwitchSate(id, 1)
+    else:
+        return None
+
 
 # args = sys.argv[1:]
 # id = args[0]
 # state = args[1]
-
+Switch_Press_Release(1)
