159dfc03508834671b86098401b77f154674ed13
Daniel Kashkett <dkashkett@gmail.com>
2021-09-26 21:21:01 -0600

    removed legacy files

--- eye_commander/face_detection/face_detection.py
+++ eye_commander/face_detection/face_detection.py
@@ -1,32 +1,26 @@
 import mediapipe as mp 
 import numpy as np
-import cv2 
 
 class FaceDetector:
-    
+
     KEYPOINTS = {'left':[159,145,33,133], 
-                 'right':[386,374,362,263], 
-                 'left_outline':[33,7,163,144,145,153,154,155,
-                                 133, 173,157,158,159,160,161,246], 
-                 'right_outline':[362,382,381,380,374,373,390,249,
-                                  263,466,388,387,386,385,384,398]}
-    
-    def __init__(self, static:bool=False, 
-                 min_detection_confidence:float=0.7, 
-                 min_tracking_confidence:float=0.7):
-        
+                  'right':[386,374,362,263]}
+
+    def __init__(self, static:bool=True, min_detection_confidence:float=0.4, min_tracking_confidence:float=0.5):
+
         self.face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=static, 
-                                                         max_num_faces=1,
-                                                         min_detection_confidence=min_detection_confidence, 
-                                                         min_tracking_confidence=min_tracking_confidence)
+                                        max_num_faces=1,
+                                        min_detection_confidence=min_detection_confidence, 
+                                        min_tracking_confidence=min_tracking_confidence)
         self.input_shape = None
         self.landmarks = None
-        self.left_keys = None
-        self.right_keys = None
-        self.left_outline = None
-        self.right_outline = None
-        self.mask = None
-        
+        self.left_keys = [159,145,33,133]
+        self.right_keys = [386,374,362,263]
+
+    @staticmethod
+    def _get_xy(point, input_shape):
+        return int(point.x * input_shape[1]), int(point.y * input_shape[0])
+      
     def _detect(self, frame: np.array):
         """Wrapper for mediapipe's process method. Detects face mesh landmarks.
 
@@ -42,42 +36,26 @@ class FaceDetector:
         
         if results.multi_face_landmarks:
             
-            detection = results.multi_face_landmarks[0].landmark
+            landmarks = results.multi_face_landmarks[0]
             
-            return detection
+            return landmarks
     
-    def _get_coordinates(self, detection):
-        """_get_coordinates processes the detection object and extracts
+    def _get_coordinates(self, landmarks):
+        """_get_coordinates processes the landmarks list and extracts
         the useful coordinates for eyes
 
         Args:
-            detection object: mediapipe detection object
-        """
-        
-        self.landmarks = []
-        for point in detection:
-            self.landmarks.append([int(point.x * self.input_shape[1]), int(point.y * self.input_shape[0])])
+            landmark list: normalized landmark list
+          """
+        left_normalized_points = [landmarks.landmark[i] for i in self.KEYPOINTS['left']]
+        right_normalized_points = [landmarks.landmark[i] for i in self.KEYPOINTS['right']]
 
-        self.left_keys = []
-        for point in self.KEYPOINTS['left']:
-            self.left_keys.append(self.landmarks[point])
-        
-        self.right_keys = []   
-        for point in self.KEYPOINTS['right']:
-            self.right_keys.append(self.landmarks[point])
-        
-        self.left_outline = []
-        for point in self.KEYPOINTS['left_outline']:
-            self.left_outline.append(self.landmarks[point])
-        
-        self.right_outline = []
-        for point in self.KEYPOINTS['right_outline']:
-            self.right_outline.append(self.landmarks[point])
-          
-        self.left_outline = np.array([self.left_outline], dtype=np.int32)
-        self.right_outline = np.array([self.right_outline], dtype=np.int32) 
+        left_coords = [self._get_xy(point, self.input_shape) for point in left_normalized_points]
+        right_coords = [self._get_xy(point, self.input_shape) for point in right_normalized_points]
+
+        return (left_coords, right_coords)
     
-    def _crop_images(self, frame:np.array):
+    def _crop_images(self, frame:np.array, coords:tuple):
         """_crop_images uses the useful coordinates from the _get_coordinates
         method to crop the frame into left and right eye images
 
@@ -87,35 +65,19 @@ class FaceDetector:
         Returns:
             tuple: tuple of images (left_eye, right_eye)
         """
+        left_coords, right_coords = coords
         k=15 
         j=10
         #### left eye
-        y1, y2, x1, x2 = self.left_keys
+        y1, y2, x1, x2 = left_coords
         eye_left = frame[y1[1]-k:y2[1]+k, x1[0]-j:x2[0]+j]
         
         #### right eye
-        y1, y2, x1, x2 = self.right_keys
+        y1, y2, x1, x2 = right_coords
         eye_right = frame[y1[1]-k:y2[1]+k, x1[0]-j:x2[0]+j]
 
         return (eye_left, eye_right)
-    
-    def generate_mask(self, frame:np.array):
-        mask = np.zeros(frame.shape[:2], dtype=np.uint8)
-        mask = cv2.fillConvexPoly(mask, self.left_outline,255)
-        mask = cv2.fillConvexPoly(mask, self.right_outline,255)
-        kernel = np.ones((9, 9), np.uint8)
-        mask = cv2.dilate(mask, kernel, 5)
-        mask = self._crop_images(mask)
-        return mask
-    
-    def _mask(self,frame):
-        mask = np.zeros(frame.shape[:2], dtype=np.uint8)
-        mask = cv2.fillConvexPoly(mask, self.left_outline,255)
-        mask = cv2.fillConvexPoly(mask, self.right_outline,255)
-        kernel = np.ones((9, 9), np.uint8)
-        mask = cv2.dilate(mask, kernel, 5)
-        return mask
-    
+
     def eyes(self, frame:np.array):
         """eyes uses the coordinates found by mediapipe's facemesh to crop 
         the left and right eye.
@@ -126,9 +88,8 @@ class FaceDetector:
         Returns:
             tuple: (left,right) eye images
         """
-        detection = self._detect(frame)  
-        if detection:
-            self._get_coordinates(detection)
-            eyes = self._crop_images(frame)
+        landmarks = self._detect(frame)  
+        if landmarks:
+            coords = self._get_coordinates(landmarks)
+            eyes = self._crop_images(frame, coords)
             return eyes
-      
--- eye_commander/models/models.py
+++ eye_commander/models/models.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 from operator import itemgetter
 from eye_commander.preprocessing import preprocessing
+import pickle
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 class CNNModel:
@@ -35,6 +36,94 @@ class CNNModel:
         
         # combine left/right back into one batch
         batch = np.concatenate([left,right])
+        
+        return batch
+    
+    def predict(self, images: tuple):
+        """ uses prediction vectors from tensorflow model, selects and outputs the 
+        prediction with the highest probability. input is the (left,right) eye image tuple.
+
+        Args:
+            images (tuple): tuple of array image data for left and right eye
+
+        Returns:
+            prediction (int): predicted class
+            probability (float): probability for predicted class
+        """
+        # make batch
+        batch = self._prep_batch(images)
+        
+        # make prediction on batch -- returns two vectors
+        pred_vecs = self.model.predict(batch)
+        
+        #### select the prediction from the eye with higher probability
+        left_pred = (pred_vecs[0].max(), pred_vecs[0].argmax())
+        
+        right_pred = (pred_vecs[1].max(), pred_vecs[1].argmax())
+        
+        probability, prediction = max([left_pred, right_pred], key=itemgetter(0))
+        
+        return prediction, probability
+    
+    def tune(self, X, y):
+        """ tunes self.model to user image data
+
+        Args:
+            X (np.array): array of images
+            y (np.array): array of labels
+        """
+        #### prep model
+        # make all but the output layer untrainable
+        for layer in self.model.layers:
+            
+            layer.trainable = False
+            
+        self.model.layers[-1].trainable = True
+        
+        #### prep data
+        X = self.image_processor.preprocess(X)
+        
+        X = [np.expand_dims(img,0) for img in X]
+        
+        X = np.concatenate(X)
+        
+        y = np.array(y)
+        
+        #### tune model
+        self.model.fit(x= X, y=y, epochs=7, batch_size=5, shuffle=True)
+        
+
+class RandomForrestModel:
+    
+    PATH = os.path.join(os.getcwd(),'eye_commander/models/trained_models/cnn_filtered.h5')
+    
+    def __init__(self):
+    
+        self.model = pickle.load(open(self.PATH, 'rb'))
+        self.tuned_model = None
+        self.image_processor = preprocessing.ImageProcessor()
+
+    def _prep_batch(self, images: tuple):
+        """ preprocesses and reshapes data to correct format for 
+        tensorflow inference
+
+        Args:
+            images (tuple): tuple consisting of left,right eye images   
+
+        Returns:
+            np.array : np.array with dimensions (2, h, w)
+        """
+        # preprocessing 
+        eyes = self.image_processor.preprocess(images)
+        
+        # add batch dimension for tensorflow
+        left = np.expand_dims(eyes[0], 0)
+        right = np.expand_dims(eyes[1], 0)
+        
+        # combine left/right back into one batch
+        batch = np.concatenate([left,right])
+        
+        batch = np.reshape(batch, (-1, batch.shape[1]*batch.shape[2]))
         
         return batch
     
