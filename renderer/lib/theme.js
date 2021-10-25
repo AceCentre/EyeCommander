3fe3a90e869216c273e99ff8695e6ef1fdedc47e
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-10-25 14:43:08 +0100

    Update stuff

--- renderer/lib/theme.js
+++ renderer/lib/theme.js
@@ -1,19 +1,19 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
+import { createTheme } from "@material-ui/core/styles";
+import red from "@material-ui/core/colors/red";
 
-export const theme = createMuiTheme({
+export const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#556cd6",
     },
     secondary: {
-      main: '#19857b',
+      main: "#19857b",
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: "#fff",
     },
   },
 });
