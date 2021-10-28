26c3afd1f00cbef0e2915dcdeaea937df89e761e
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-10-28 12:19:10 +0100

    Forge demo

--- forge-demo/src/react-app/index.jsx
+++ forge-demo/src/react-app/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import FaceMeshComponent from "./face-mesh.jsx";
 
 export const Home = () => {
-  return <h1>My Home</h1>;
+  return (
+    <div>
+      <h1>My Home</h1>
+      <FaceMeshComponent />
+    </div>
+  );
 };
--- forge-demo/webpack.renderer.config.js
+++ forge-demo/webpack.renderer.config.js
@@ -1,8 +1,11 @@
-const rules = require('./webpack.rules');
+const path = require("path");
+const rules = require("./webpack.rules");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const assets = ["public"]; // asset directories
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
 });
 
 module.exports = {
@@ -10,4 +13,14 @@ module.exports = {
   module: {
     rules,
   },
+  plugins: assets.map((asset) => {
+    return new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", asset),
+          to: path.resolve(__dirname, ".webpack/renderer", asset),
+        },
+      ],
+    });
+  }),
 };
