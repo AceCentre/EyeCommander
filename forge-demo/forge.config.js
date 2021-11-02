2e930f8b7377b6716cbd91223f46ff61ad502b65
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-11-02 14:25:30 +0000

    Try version 12

---
+++ forge-demo/forge.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+,
+  packagerConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "EyeCommander",
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+  plugins: [
+    [
+      "@electron-forge/plugin-webpack",
+      {
+        mainConfig: "./webpack.main.config.js",
+        renderer: {
+          config: "./webpack.renderer.config.js",
+          entryPoints: [
+            {
+              html: "./src/index.html",
+              js: "./src/renderer.js",
+              name: "main_window",
+              preload: {
+                js: "./src/preload.js",
+              },
+            },
+          ],
+        },
+      },
+    ],
+  ],
+};
