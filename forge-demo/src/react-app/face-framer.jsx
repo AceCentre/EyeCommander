b3d0a07ff2bacf8ddcf7c2f64b686a8c0af08269
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-11-02 16:41:54 +0000

    Add face framing

--- forge-demo/src/react-app/face-framer.jsx
+++ forge-demo/src/react-app/face-framer.jsx
@@ -1,6 +1,6 @@
-import { Button, Paper } from "@mui/material";
+import { Button, Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Camera } from "@mediapipe/camera_utils";
 import { drawConnectors } from "@mediapipe/drawing_utils";
 import {
@@ -8,14 +8,18 @@ import {
   FACEMESH_RIGHT_EYE,
   FACEMESH_LEFT_EYE,
   FACEMESH_FACE_OVAL,
+  FACEMESH_RIGHT_IRIS,
+  FACEMESH_LEFT_IRIS,
 } from "@mediapipe/face_mesh";
 
 import { SelectedWebcam } from "./selected-webcam.jsx";
 import { useLoading } from "./hooks/use-loading.js";
+import { red } from "@mui/material/colors";
 
 export const FaceFramer = ({ nextTask, prevTask }) => {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const [allowNext, setAllowNext] = useState(true);
   const loading = useLoading(500);
 
   function onResults(results) {
@@ -37,13 +41,28 @@ export const FaceFramer = ({ nextTask, prevTask }) => {
       canvasElement.width,
       canvasElement.height
     );
+
+    if (results.multiFaceLandmarks.length === 1) {
+      setAllowNext(true);
+    } else {
+      setAllowNext(false);
+    }
+
     if (results.multiFaceLandmarks) {
       for (const landmarks of results.multiFaceLandmarks) {
         drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {
           color: "#FF3030",
         });
 
+        drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, {
+          color: "#FF3030",
+        });
+
         drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, {
+          color: "#30FF30",
+        });
+
+        drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, {
           color: "#30FF30",
         });
 
@@ -92,13 +111,26 @@ export const FaceFramer = ({ nextTask, prevTask }) => {
         maxWidth: "540px",
       }}
     >
+      {!allowNext && (
+        <Box>
+          <Typography sx={{ color: red[500] }}>
+            We cannot find a face in the window. Try getting closer to the
+            webcam or improve the lighting of your face.
+          </Typography>
+        </Box>
+      )}
       <SelectedWebcam sx={{ display: "none" }} webcamRef={webcamRef} />
       <canvas ref={canvasRef} />
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         <Button onClick={prevTask} size="large">
           Previous step
         </Button>
-        <Button onClick={nextTask} disabled size="large" variant="contained">
+        <Button
+          onClick={nextTask}
+          disabled={!allowNext}
+          size="large"
+          variant="contained"
+        >
           Next step
         </Button>
       </Box>
