e323a953539da24982d8fbff1fbb43a4cf0db948
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-11-15 09:34:47 +0000

    Link ffi-napi on windows

--- setup-win-deps.js
+++ setup-win-deps.js
@@ -2,27 +2,11 @@
 // If we are not we want to mock it
 
 const os = require("os");
-const fs = require("fs");
-const path = require("path");
-
-const VERSION = "4.0.3";
+const { execSync } = require("child_process");
 
 console.log("======= Setup Win Deps ========");
 
-const packageJson = require("./package.json");
-const { execSync } = require("child_process");
-const newPackageJson = {
-  ...packageJson,
-  dependencies: { ...packageJson.dependencies, "ffi-napi": VERSION },
-};
-
-if (os.platform() === "win32") {
-  console.log("Adding 'ffi-napii' to package.json");
-  fs.writeFileSync(
-    path.join(__dirname, "./package.json"),
-    JSON.stringify(newPackageJson, null, 2) + os.EOL
-  );
-} else {
+if (os.platform() !== "win32") {
   console.log("Stubbing ffi-napi");
   execSync("cd ./fake-ffi-napi && yarn link && cd ../ && yarn link ffi-napi", {
     stdio: "inherit",
