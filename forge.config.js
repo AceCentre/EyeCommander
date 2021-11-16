22abf1cc5652b7fb5daafc3035e64789ad8b9fc1
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-11-16 10:57:17 +0000

    Try and build with windows flag

--- forge.config.js
+++ forge.config.js
@@ -77,6 +77,9 @@ module.exports = {
   packagerConfig: {
     icon,
     ...osxSigning,
+    win32metadata: {
+      'requested-execution-level': 'requireAdministratorl'
+    }
   },
   publishers,
   makers: [
