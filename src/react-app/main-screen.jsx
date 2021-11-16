17d5766f03ff899bed32564909efd1c615a5b0d7
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-11-16 14:53:05 +0000

    Add pause button

--- src/react-app/main-screen.jsx
+++ src/react-app/main-screen.jsx
@@ -13,6 +13,8 @@ import { useOpenSettings } from "./hooks/use-open-settings";
 import { useReload } from "./hooks/use-reload";
 import { useStoreValue } from "./hooks/use-store";
 import { PLAY_SOUND } from "./lib/store-consts";
+import PauseIcon from "@mui/icons-material/Pause";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 const CustomWidthTooltip = styled(({ className, ...props }) => (
   <Tooltip {...props} classes={{ popper: className }} />
@@ -28,6 +30,7 @@ export const MainScreen = () => {
     PLAY_SOUND,
     true
   );
+  const [paused, setPaused] = useState(false);
   const reloadTrigger = useReload([reloadPlaySound]);
   const [play] = useSound("./public/notif.mp3");
   const [isFaceInFrame, setIsFaceInFrame] = useState(true);
@@ -38,8 +41,12 @@ export const MainScreen = () => {
     if (playSound) {
       play();
     }
-    sendBlinkToBackend();
-  }, [play, playSound, reloadTrigger]);
+
+    if (!paused) {
+      console.log("sending blink");
+      sendBlinkToBackend();
+    }
+  }, [play, playSound, reloadTrigger, paused]);
 
   if (reloadTrigger % 2 !== 0) {
     return null;
@@ -60,7 +67,14 @@ export const MainScreen = () => {
               the screen and well lit.
             </Typography>
           )}
-          <Box sx={{ marginTop: "auto" }}>
+          <Box
+            sx={{
+              marginTop: "auto",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
             <CustomWidthTooltip
               title="Change the settings including controlling the output."
               placement="bottom"
@@ -68,13 +82,44 @@ export const MainScreen = () => {
               enterNextDelay={1000}
             >
               <Button
-                variant="contained"
+                variant="outlined"
                 startIcon={<SettingsIcon />}
                 onClick={openSettings}
               >
                 Settings
               </Button>
             </CustomWidthTooltip>
+            {paused ? (
+              <CustomWidthTooltip
+                title="Continue outputting to the chosen output method"
+                placement="bottom"
+                enterDelay={1000}
+                enterNextDelay={1000}
+              >
+                <Button
+                  variant="contained"
+                  startIcon={<PlayArrowIcon />}
+                  onClick={() => setPaused(false)}
+                >
+                  Resume
+                </Button>
+              </CustomWidthTooltip>
+            ) : (
+              <CustomWidthTooltip
+                title="Stop outputting to chosen output method"
+                placement="bottom"
+                enterDelay={1000}
+                enterNextDelay={1000}
+              >
+                <Button
+                  variant="contained"
+                  startIcon={<PauseIcon />}
+                  onClick={() => setPaused(true)}
+                >
+                  Pause
+                </Button>
+              </CustomWidthTooltip>
+            )}
           </Box>
         </>
       </BlinkDetectionWithSliders>
