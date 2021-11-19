b2c4a803f5484aa59db0a28f83ebb589aadb36fc
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-11-19 11:48:18 +0000

    Increase window size

--- src/react-app/settings-page.jsx
+++ src/react-app/settings-page.jsx
@@ -36,7 +36,7 @@ const SCREENS = {
 };
 
 export const SettingsPage = () => {
-  useResizer({ width: 900, height: 600 });
+  useResizer({ width: 910, height: 620 });
   const saveAndClose = useSaveAndClose();
   const [currentScreen, setCurrentScreen] = useState(SCREENS.CAMERA);
 
