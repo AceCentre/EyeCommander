41e81452344821c254dd3a26a72d6c10086287df
Gavin Henderson <ghenderson@acecentre.org.uk>
2021-12-17 16:14:49 +0000

    Try loading vb scripts

--- src/backend/squirrel-events.js
+++ src/backend/squirrel-events.js
@@ -3,9 +3,10 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 var app = require('electron').app;
 var logger = require('electron-log')
-var regedit = require('regedit').promisified;
+var regeditRaw = require('regedit');
+var regedit = regeditRaw.promisified
 
-var run = function(args, done) {
+var run = function (args, done) {
   var updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
   logger.info('Spawning `%s` with args `%s`', updateExe, args);
   spawn(updateExe, args, {
@@ -13,25 +14,36 @@ var run = function(args, done) {
   }).on('close', done);
 };
 
-var check = async function() {
+var check = async function () {
   if (process.platform === 'win32') {
     var cmd = process.argv[1];
     logger.info('processing squirrel command `%s`', cmd);
     var target = path.basename(process.execPath);
     console.log("\n===== MAKING REG EDIT ======")
     const keyPath = 'HKCU\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers'
-    console.log({ target: process.execPath, keyPath })
+    const execPath = process.execPath.includes('electron.exe') ? 'demoPath' : process.execPath
+    const pathToVbs = path.resolve(__dirname, "..", "main", 'vbs');
+
+    console.log({ target: execPath, keyPath, externalResourcePath: pathToVbs })
 
     try {
+      const result = regeditRaw.setExternalVBSLocation(pathToVbs);
+      if(result == 'Folder not found'){
+        throw new Error('Couldnt find vb scripts');
+      }
+      console.log(result)
+
       await regedit.createKey(keyPath);
-      await regedit.putValue({[keyPath]: {
-        [process.execPath]: {
-          value: '~RUNASADMIN',
-          type: "REG_SZ"
+      await regedit.putValue({
+        [keyPath]: {
+          [execPath]: {
+            value: '~RUNASADMIN',
+            type: "REG_SZ"
+          }
         }
-      }})
+      })
       console.log("===== MADE EDIT SUCCESSFULLY =====")
-    } catch(error) {
+    } catch (error) {
       console.log(error);
       console.log("===== FAILED TO MAKE EDIT =====")
 
