42140358fa7dce2c64e49b59bcc02efdf29378f7
Gavin Henderson <ghenderson@acecentre.org.uk>
2021-12-17 14:46:24 +0000

    Make regedit at the start of every startup

--- src/backend/squirrel-events.js
+++ src/backend/squirrel-events.js
@@ -3,6 +3,7 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 var app = require('electron').app;
 var logger = require('electron-log')
+var regedit = require('regedit').promisified;
 
 var run = function(args, done) {
   var updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
@@ -12,11 +13,29 @@ var run = function(args, done) {
   }).on('close', done);
 };
 
-var check = function() {
+var check = async function() {
   if (process.platform === 'win32') {
     var cmd = process.argv[1];
     logger.info('processing squirrel command `%s`', cmd);
     var target = path.basename(process.execPath);
+    console.log("\n===== MAKING REG EDIT ======")
+    const keyPath = 'HKCU\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers'
+    console.log({ target: process.execPath, keyPath })
+
+    try {
+      await regedit.createKey(keyPath);
+      await regedit.putValue({[keyPath]: {
+        [process.execPath]: {
+          value: '~RUNASADMIN',
+          type: "REG_SZ"
+        }
+      }})
+      console.log("===== MADE EDIT SUCCESSFULLY =====")
+    } catch(error) {
+      console.log(error);
+      console.log("===== FAILED TO MAKE EDIT =====")
+
+    }
 
     if (cmd === '--squirrel-install' || cmd === '--squirrel-updated') {
       run(['--createShortcut=' + target + ''], app.quit);
