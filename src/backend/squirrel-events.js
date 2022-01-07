5f8c78c42ce8713307746f4aec0b94650f2739ae
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-01-07 15:48:59 +0000

    Clean up squirrel events

--- src/backend/squirrel-events.js
+++ src/backend/squirrel-events.js
@@ -1,56 +1,61 @@
+const path = require("path");
+const spawn = require("child_process").spawn;
+const app = require("electron").app;
+const logger = require("electron-log");
+const regeditRaw = require("regedit");
+const regedit = regeditRaw.promisified;
 
-var path = require('path');
-var spawn = require('child_process').spawn;
-var app = require('electron').app;
-var logger = require('electron-log')
-var regeditRaw = require('regedit');
-var regedit = regeditRaw.promisified
-
-var run = function (args, done) {
-  var updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
-  logger.info('Spawning `%s` with args `%s`', updateExe, args);
+const run = function (args, done) {
+  const updateExe = path.resolve(
+    path.dirname(process.execPath),
+    "..",
+    "Update.exe"
+  );
+  logger.info("Spawning `%s` with args `%s`", updateExe, args);
   spawn(updateExe, args, {
-    detached: true
-  }).on('close', done);
+    detached: true,
+  }).on("close", done);
 };
 
-var check = async function () {
-  if (process.platform === 'win32') {
-    var cmd = process.argv[1];
-    logger.info('processing squirrel command `%s`', cmd);
-    var target = path.basename(process.execPath);
-    logger.info("\n===== MAKING REG EDIT ======")
-    const keyPath = 'HKCU\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers'
-    const execPath = process.execPath.includes('electron.exe') ? `localDevPath-${new Date().toISOString()}` : process.execPath
+const check = async function () {
+  if (process.platform === "win32") {
+    const cmd = process.argv[1];
+    logger.info("processing squirrel command `%s`", cmd);
+    const target = path.basename(process.execPath);
+    logger.info("\n===== MAKING REG EDIT ======");
+    const keyPath =
+      "HKCU\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers";
+    const execPath = process.execPath.includes("electron.exe")
+      ? `localDevPath-${new Date().toISOString()}`
+      : process.execPath;
+
+    if (cmd === "--squirrel-install" || cmd === "--squirrel-updated") {
+      run(["--createShortcut=" + target + ""], app.quit);
+      return true;
+    }
+    if (cmd === "--squirrel-uninstall") {
+      run(["--removeShortcut=" + target + ""], app.quit);
+      return true;
+    }
+    if (cmd === "--squirrel-obsolete") {
+      app.quit();
+      return true;
+    }
 
     try {
       await regedit.createKey(keyPath);
       await regedit.putValue({
         [keyPath]: {
           [execPath]: {
-            value: '~RUNASADMIN',
-            type: "REG_SZ"
-          }
-        }
-      })
-      logger.info("===== MADE EDIT SUCCESSFULLY =====")
+            value: "~RUNASADMIN",
+            type: "REG_SZ",
+          },
+        },
+      });
+      logger.info("===== MADE EDIT SUCCESSFULLY =====");
     } catch (error) {
       logger.info(error);
-      logger.info("===== FAILED TO MAKE EDIT =====")
-
-    }
-
-    if (cmd === '--squirrel-install' || cmd === '--squirrel-updated') {
-      run(['--createShortcut=' + target + ''], app.quit);
-      return true;
-    }
-    if (cmd === '--squirrel-uninstall') {
-      run(['--removeShortcut=' + target + ''], app.quit);
-      return true;
-    }
-    if (cmd === '--squirrel-obsolete') {
-      app.quit();
-      return true;
+      logger.info("===== FAILED TO MAKE EDIT =====");
     }
   }
   return false;
