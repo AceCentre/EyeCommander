c1a2dcea6dc140c60a21c13f86d61f3153042ec5
Gavin Henderson <ghenderson@acecentre.org.uk>
2022-01-07 14:25:36 +0000

    Use published fork

--- webpack.rules.js
+++ webpack.rules.js
@@ -10,7 +10,7 @@ module.exports = [
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: "@zeit/webpack-asset-relocator-loader",
+      loader: "@marshallofsound/webpack-asset-relocator-loader",
       options: {
         outputAssetBase: "native_modules",
         debugLog: true,
