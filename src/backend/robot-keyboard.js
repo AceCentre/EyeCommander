45905ebf38ed0a818c00cbacf959fb408978acb4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-03-25 17:04:47 +0000

    Allow changing key from store

--- src/backend/robot-keyboard.js
+++ src/backend/robot-keyboard.js
@@ -1,24 +1,39 @@
 import { AbstractOutput } from "./abstract-output";
 import sendkeys from "sendkeys";
+import { WEB_KEY_TO_OS_KEY } from "../web-key-to-os-key";
 
+const getWithDefault = (store, id, defaultVal) => {
+  const val = store.get(id);
+
+  if (val !== undefined) return val;
+
+  store.set(id, defaultVal);
+
+  return defaultVal;
+};
 export class RobotKeyboard extends AbstractOutput {
-  constructor() {
+  constructor({ store }) {
     super();
+
+    this.store = store;
   }
 
   blink() {
-    sendkeys.sync(" ");
+    const webKey = getWithDefault(this.store, "CURRENT_WEB_KEY", " ");
+    const osKey = WEB_KEY_TO_OS_KEY[webKey.toLowerCase()];
+
+    sendkeys.sync(osKey);
   }
 
   static getCompatiblePlatforms() {
-    return ["win32"];
+    return ["win32", "darwin"];
   }
 
   static info() {
     return {
       name: "Keyboard Emulator",
       description:
-        "Emulates a keyboard. Outputs a 'space' keypress for a blink",
+        "Emulates a keypress. Allows you to change the key that is pressed.",
       icon: "KeyboardIcon",
     };
   }
