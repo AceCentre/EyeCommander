5879fd5367b55433bbd0bfe9b11bb78571642e8f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-08-05 12:32:45 +0100

    Chnge to mac only

--- forge.config.js
+++ forge.config.js
@@ -1,73 +1,34 @@
-const os = require("os");
+// if (os.platform() === "darwin") {
+//   icon = "./assets/apple-icon.icns";
 
-let icon = undefined;
-let osxSigning = {};
-let certParams = {};
+//   osxSigning = {
+//     osxSign: {
+//       identity: "Developer ID Application: THE ACE CENTRE-NORTH (K45HHA96ND)",
+//       "hardened-runtime": true,
+//       entitlements: "./assets/entitlements.plist",
+//       "entitlements-inherit": "./assets/entitlements.plist",
+//       "signature-flags": "library",
+//       "gatekeeper-assess": false,
+//     },
+//   };
 
-if (os.platform() === "win32") {
-  icon = "./assets/windows_icon.ico";
-
-  // if (process.env.WIN_CERT_PASSWORD) {
-  //   console.log("WINDOWS SIGNING PARAM FOUND");
-
-  //   certParams = {
-  //     certificateFile: "./win-cert.pfx",
-  //     certificatePassword: process.env.WIN_CERT_PASSWORD,
-  //     exe: "EyeCommander.exe",
-  //     setupExe: "EyeCommanderSetup.exe",
-  //   };
-  // } else {
-  //   console.log("NO WINDOWS SIGNING PARAMS");
-  // }
-}
-
-if (os.platform() === "darwin") {
-  icon = "./assets/apple-icon.icns";
-
-  osxSigning = {
-    osxSign: {
-      identity: "Developer ID Application: THE ACE CENTRE-NORTH (K45HHA96ND)",
-      "hardened-runtime": true,
-      entitlements: "./assets/entitlements.plist",
-      "entitlements-inherit": "./assets/entitlements.plist",
-      "signature-flags": "library",
-      "gatekeeper-assess": false,
-    },
-  };
-
-  console.log("=========");
-  if (
-    process.env.APPLE_ID &&
-    process.env.APPLE_ID_PASSWORD &&
-    process.env.APPLE_PROVIDER
-  ) {
-    console.log("SIGNING PARAMS ADDED");
-    osxSigning.osxNotarize = {
-      appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_ID_PASSWORD,
-      ascProvider: process.env.APPLE_PROVIDER,
-    };
-  } else {
-    console.log("NO SIGNING PARAMS IN ENV");
-  }
-  console.log("=========");
-}
-
-let publishers = [];
-
-if (process.env.GITHUB_TOKEN) {
-  publishers.push({
-    name: "@electron-forge/publisher-github",
-    config: {
-      repository: {
-        owner: "AceCentre",
-        name: "EyeCommander",
-      },
-      prerelease: false,
-      draft: false,
-    },
-  });
-}
+//   console.log("=========");
+//   if (
+//     process.env.APPLE_ID &&
+//     process.env.APPLE_ID_PASSWORD &&
+//     process.env.APPLE_PROVIDER
+//   ) {
+//     console.log("SIGNING PARAMS ADDED");
+//     osxSigning.osxNotarize = {
+//       appleId: process.env.APPLE_ID,
+//       appleIdPassword: process.env.APPLE_ID_PASSWORD,
+//       ascProvider: process.env.APPLE_PROVIDER,
+//     };
+//   } else {
+//     console.log("NO SIGNING PARAMS IN ENV");
+//   }
+//   console.log("=========");
+// }
 
 module.exports = {
   electronRebuildConfig: {
@@ -75,53 +36,16 @@ module.exports = {
     useCache: false,
   },
   packagerConfig: {
-    icon,
     executableName: "eyecommander",
-    ...osxSigning,
   },
-  publishers,
+  publishers: [],
   makers: [
-    {
-      name: "@electron-forge/maker-squirrel",
-      config: {
-        name: "EyeCommander",
-        ...certParams,
-      },
-    },
-    {
-      name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
-    },
-    {
-      name: "@electron-forge/maker-deb",
-      config: {
-        options: {
-          maintainer: "Ace Centre",
-          homepage: "https://acecentre.org.uk",
-        },
-      },
-    },
-    {
-      name: "@electron-forge/maker-rpm",
-      config: {
-        options: {
-          maintainer: "Ace Centre",
-          homepage: "https://acecentre.org.uk",
-        },
-      },
-    },
     {
       name: "@electron-forge/maker-dmg",
       config: {
         format: "ULFO",
         name: "EyeCommander",
         icon: "./assets/apple-icon.png",
-        additionalDMGOptions: {
-          "code-sign": {
-            identifier: "",
-            "signing-identity": "",
-          },
-        },
       },
     },
   ],
@@ -144,13 +68,6 @@ module.exports = {
             },
           ],
         },
-      },
-    ],
-    [
-      "@timfish/forge-externals-plugin",
-      {
-        externals: ["ffi-napi", "ref-napi"],
-        includeDeps: true,
       },
     ],
   ],
